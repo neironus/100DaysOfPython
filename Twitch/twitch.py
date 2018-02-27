9f757021284a2dd0d812020894d902448f4533d3
Unknown <JTfbVbcm1K@pas.im>
2018-02-27 13:44:16 +0100

    Add threading

--- Twitch/twitch.py
+++ Twitch/twitch.py
@@ -2,18 +2,26 @@
 import config as cfg
 from db import DB
 import urllib2, json
+import threading
 
 d = DB()
 
-channels = ['woowakgood', 'lexveldhuis', 'p4wnyhof']
+def run():
+    stats()
+    t = threading.Timer(600 ,run) #Every 5 minutes
+    t.start()
 
-for c in channels:
-    url = 'https://api.twitch.tv/kraken/streams/' + c
-    req = urllib2.Request(url)
-    req.add_header("Client-ID",cfg.twitch['client_id'])
-    resp = urllib2.urlopen(req)
-    content = json.loads(resp.read())
+def stats():
+    channels = ['woowakgood', 'lexveldhuis', 'p4wnyhof']
+    for c in channels:
+        url = 'https://api.twitch.tv/kraken/streams/' + c
+        req = urllib2.Request(url)
+        req.add_header("Client-ID",cfg.twitch['client_id'])
+        resp = urllib2.urlopen(req)
+        content = json.loads(resp.read())
 
-    if content.get('stream') and content.get('stream').get('viewers'):
-        print "%s - %d" % (c, content.get('stream').get('viewers'))
-        d.insert('viewers', {'user': c, 'viewers': content.get('stream').get('viewers')})
+        if content.get('stream') and content.get('stream').get('viewers'):
+            print "%s - %d" % (c, content.get('stream').get('viewers'))
+            d.insert('viewers', {'user': c, 'viewers': content.get('stream').get('viewers')})
+
+run()
