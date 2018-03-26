ff087f701d55a73ea098785cd52220609d043fdf
Unknown <JTfbVbcm1K@pas.im>
2018-03-26 16:39:31 +0200

    Day24
    print format

--- PyBites/Challenges/13/exitttttt/directors.py
+++ PyBites/Challenges/13/exitttttt/directors.py
@@ -22,6 +22,9 @@ def get_movies_by_director():
             except ValueError:
                 continue
 
+    for did, movies in directors.items():
+        directors[did] = sorted(movies, key=lambda x: x.score, reverse=True)
+
     return directors
 
 def get_average_scores(directors):
@@ -50,6 +53,18 @@ def print_results(directors):
     fmt_director_entry = '{counter}. {director:<52} {avg}'
     fmt_movie_entry = '{year}] {title:<50} {score}'
     sep_line = '-' * 60
+    report = sorted(directors.items(), key=lambda x: (float(x[0][1]), x[0][0] ), reverse=True)
+    i = 0
+    for director, movies in report:
+        i += 1
+        if i > NUM_TOP_DIRECTORS:
+            continue
+
+        print(fmt_director_entry.format(counter=i, director=director[0], avg=director[1]))
+        print(sep_line)
+        for movie in movies:
+            print(fmt_movie_entry.format(year=movie.year, title=movie.title, score=movie.score))
+        print(' ')
 
 
 def main():
@@ -57,7 +72,7 @@ def main():
     We wrote some tests based on our solution: test_directors.py'''
     directors = get_movies_by_director()
     directors = get_average_scores(directors)
-    print(directors)
+    # print(directors)
     print_results(directors)
 
 
