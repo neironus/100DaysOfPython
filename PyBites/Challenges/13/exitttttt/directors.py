da23a952fda9604ee1cb76387c125bfb995cda29
Unknown <JTfbVbcm1K@pas.im>
2018-03-27 15:37:49 +0200

    Day 25
    Lambda, List Comprehensions

--- PyBites/Challenges/13/exitttttt/directors.py
+++ PyBites/Challenges/13/exitttttt/directors.py
@@ -29,21 +29,10 @@ def get_movies_by_director():
 
 def get_average_scores(directors):
     '''Filter directors with < MIN_MOVIES and calculate averge score'''
-    did = defaultdict(list)
-
-    for director, movies in directors.items():
-        if len(movies) >= MIN_MOVIES:
-            did[(director, _calc_mean(movies))] = movies
-
-    return did
+    return {(directors,_calc_mean(movies)):movies for directors, movies in directors.items() if len(movies) >= MIN_MOVIES}
 
 def _calc_mean(movies):
-    score = 0
-
-    for movie in movies:
-        score += movie.score
-
-    return round(score / len(movies), 1)
+    return round(sum(movie.score for movie in movies) / len(movies), 1)
 
 
 def print_results(directors):
@@ -53,18 +42,15 @@ def print_results(directors):
     fmt_director_entry = '{counter}. {director:<52} {avg}'
     fmt_movie_entry = '{year}] {title:<50} {score}'
     sep_line = '-' * 60
-    report = sorted(directors.items(), key=lambda x: (float(x[0][1]), x[0][0] ), reverse=True)
-    i = 0
-    for director, movies in report:
-        i += 1
-        if i > NUM_TOP_DIRECTORS:
-            continue
+    report = sorted(directors.items(), key=lambda x: (float(x[0][1]), x[0][0]) , reverse=True)[:NUM_TOP_DIRECTORS]
 
-        print(fmt_director_entry.format(counter=i, director=director[0], avg=director[1]))
+    for i, (director, movies) in enumerate(report):
+        print(' ')
+        print(fmt_director_entry.format(counter=i+1, director=director[0], avg=director[1]))
         print(sep_line)
         for movie in movies:
             print(fmt_movie_entry.format(year=movie.year, title=movie.title, score=movie.score))
-        print(' ')
+
 
 
 def main():
