6be8380bea37f457544c37fd4788eed3e943fb30
Unknown <JTfbVbcm1K@pas.im>
2018-04-07 19:07:36 +0200

    Alphabetical ordres class

--- TalkPython/15/actors.py
+++ TalkPython/15/actors.py
@@ -1,15 +1,3 @@
-class Roll:
-    def __str__(self):
-        return self.name
-
-
-    def __init__(self, name, defeated_by):
-        self.name = name
-        self.defeated_by = defeated_by
-
-    def can_defeat(self, roll):
-        return self.name in roll.defeated_by
-
 class Player:
     def __init__(self, name):
         self.name = name
@@ -17,3 +5,14 @@ class Player:
 
     def get_point(self):
         self.points += 1
+
+class Roll:
+    def __str__(self):
+        return self.name
+
+    def __init__(self, name, defeated_by):
+        self.name = name
+        self.defeated_by = defeated_by
+
+    def can_defeat(self, roll):
+        return self.name in roll.defeated_by
