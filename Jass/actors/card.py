51557c94e2b2ef2c3da109e7d8282ee577721104
Unknown <JTfbVbcm1K@pas.im>
2018-04-11 22:04:17 +0200

    add symbol to card

--- Jass/actors/card.py
+++ Jass/actors/card.py
@@ -2,9 +2,33 @@ class Card(object):
 
     # Init
     def __init__(self, color, rank):
-        self.color = color
-        self.rank = rank
+        self._color = color
+        self._rank = rank
 
     # Representation of card
     def __repr__(self):
-        return 'Color {} - Rank {}'.format(self.color, self.rank)
+        return 'Color {} - Rank {}'.format(self.color, self.symbol)
+
+    # color GET
+    @property
+    def color(self):
+        return self._color
+
+    # rank GET
+    @property
+    def rank(self):
+        return self._rank
+
+    # symbol GET
+    @property
+    def symbol(self):
+        if self.rank == 14:
+            return 'A'
+        elif self.rank == 13:
+            return 'K'
+        elif self.rank == 12:
+            return 'Q'
+        elif self.rank == 11:
+            return 'J'
+        else:
+            return self.rank
