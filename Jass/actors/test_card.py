0f2b578856b8244e30e13a2a6b4bb61d83d793dc
Unknown <JTfbVbcm1K@pas.im>
2018-04-11 22:11:01 +0200

    test symbol card

--- Jass/actors/test_card.py
+++ Jass/actors/test_card.py
@@ -1,5 +1,13 @@
 from actors.card import Card
 
 
-def test_card():
-    assert True
+def test_symbol_card():
+    # good
+    card1 = Card('Y', 7)
+    assert card1.symbol == 7
+    # good
+    card2 = Card('B', 10)
+    assert card2.symbol == 10
+    # good
+    card3 = Card('B', 14)  # Aces
+    assert card3.symbol == 'A'
