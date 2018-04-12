ec534a2706f767741ce1e44c3ab21040b131f639
Unknown <JTfbVbcm1K@pas.im>
2018-04-12 15:13:53 +0200

    Reorder if first round
    players[0] must be the one having D7

--- Jass/actors/round.py
+++ Jass/actors/round.py
@@ -8,11 +8,14 @@ class Round(object):
     # Init
     def __init__(self, roundNumber, players):
         self.roundNumber = roundNumber
-        self.players = itertools.cycle(players)
+        self.players = players
 
     # Create a new round
     def new_round(self):
         self.deal_players_cards(self._create_deck)
+
+        if (self.roundNumber == 0):
+            self._reorder_players_for_first_round()
 
     # Deal players cards
     def deal_players_cards(self, deck):
@@ -25,7 +28,7 @@ class Round(object):
     def _create_deck(self):
         deck = []
         colors = 'H D C S'.split()
-        ranks = '6 7 8 9 10 J Q K A'.split()
+        ranks = [6, 7, 8, 9, 10, 11, 12, 13, 14]
 
         for color in colors:
             for rank in ranks:
@@ -34,3 +37,20 @@ class Round(object):
         random.shuffle(deck)
 
         return deck
+
+    def _turn_order(self):
+        player = self.players.pop(0)
+        self.players.append(player)
+
+    # Reorder until players[0] have D7
+    def _reorder_players_for_first_round(self):
+        player = self.players[0]
+        print(player.hand)
+
+        if list(filter(
+            lambda x: (x.color == 'D' and x.rank == 7), player.hand
+        )):
+            print(player.name, player.hand)
+        else:
+            self._turn_order()
+            self._reorder_players_for_first_round()
