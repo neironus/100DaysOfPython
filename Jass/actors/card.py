1b4a606ee033f22bbf3ff37695733a2961941ad3
Unknown <JTfbVbcm1K@pas.im>
2018-04-12 14:25:37 +0200

    Revert "Use __str__ instead of __repr__"
    This reverts commit ad0e6e25547edf29f48fd024acda56a6d2d77207.

--- Jass/actors/card.py
+++ Jass/actors/card.py
@@ -5,8 +5,8 @@ class Card(object):
         self._color = color
         self._rank = rank
 
-    # String of card
-    def __str__(self):
+    # Representation of card
+    def __repr__(self):
         return 'Color {} - Rank {}'.format(self.color, self.symbol)
 
     # color GET
