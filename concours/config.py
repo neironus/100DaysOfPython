aa86c7217591b512fe308e66b9d865f3070c49c6
exitttttt <JTfbVbcm1K@pas.im>
2018-04-25 15:34:39 +0200

    Update config.py

--- concours/config.py
+++ concours/config.py
@@ -1,1 +1,6 @@
-twitter_key = ''
+twitter = {
+    'consumer_key': 'YOUR CONSUMER KEY',
+    'consumer_secret': 'YOUR CONSUMER SECRET',
+    'access_token': 'ACCESS TOKEN',
+    'access_token_secret': 'ACCESS TOKEN SECRET'
+}
