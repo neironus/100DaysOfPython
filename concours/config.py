b735a7ed8533ceb9fcd3958891bf589d6b7ed423
Unknown <JTfbVbcm1K@pas.im>
2018-04-26 14:14:41 +0200

    add reply option in config

--- concours/config.py
+++ concours/config.py
@@ -4,7 +4,9 @@ twitter = {
     'consumer_key': 'YOUR CONSUMER KEY',
     'consumer_secret': 'YOUR CONSUMER SECRET',
     'access_token': 'ACCESS TOKEN',
-    'access_token_secret': 'ACCESS TOKEN SECRET'
+    'access_token_secret': 'ACCESS TOKEN SECRET',
+    'sleep': True,
+    'reply': 'never',  # Possible answer never/sometime/always
 }
 
 mongodb = {
