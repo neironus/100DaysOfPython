8424779727b181e4dba4b230b6535d1f1bfe925a
exitttttt <JTfbVbcm1K@pas.im>
2018-04-26 12:54:52 +0200

    Update config.py
    Add debug, sleep and mongodb url

--- concours/config.py
+++ concours/config.py
@@ -1,6 +1,13 @@
+debug = True
+
 twitter = {
     'consumer_key': 'YOUR CONSUMER KEY',
     'consumer_secret': 'YOUR CONSUMER SECRET',
     'access_token': 'ACCESS TOKEN',
     'access_token_secret': 'ACCESS TOKEN SECRET'
+}
+
+mongodb = {
+    'host': 'mongodb://localhost:27017/',
+    'db': 'concours'
 }
