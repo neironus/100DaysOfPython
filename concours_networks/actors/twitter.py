839045c9639e866f41c3c16ea606050210b14bbf
Unknown <JTfbVbcm1K@pas.im>
2018-05-04 17:14:07 +0200

    Init concours networks

---
+++ concours_networks/actors/twitter.py
@@ -0,0 +1,6 @@
+import twitter
+
+
+class Twitter(object):
+    def __init__(self):
+        pass
---
+++ concours_networks/program.py
@@ -0,0 +1,9 @@
+from actors.twitter import Twitter
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
