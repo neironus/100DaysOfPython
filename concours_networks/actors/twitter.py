b5704921628d0610cbafa1f1b424a6b94bb212dc
Unknown <JTfbVbcm1K@pas.im>
2018-05-04 20:36:38 +0200

    networks

--- concours_networks/actors/twitter.py
+++ concours_networks/actors/twitter.py
@@ -1,6 +1,23 @@
 import twitter
+import re
 
 
 class Twitter(object):
-    def __init__(self):
-        pass
+    def __init__(self, key, secret, token, token_secret):
+        self.api = twitter.Api(
+            consumer_key=key,
+            consumer_secret=secret,
+            access_token_key=token,
+            access_token_secret=token_secret,
+            sleep_on_rate_limit=True
+        )
+
+    def interesting_user(self, user):
+        return self.does_contain_keywords(user)
+
+    def does_contain_keywords(self, user):
+        return re.search('#(.+)NoFake|legit', user.description)
+
+    def search(self, user_id):
+        followers = self.api.GetFollowers(user_id=user_id)
+        return [user.id for user in followers if self.interesting_user(user)]
--- concours_networks/program.py
+++ concours_networks/program.py
@@ -1,8 +1,37 @@
 from actors.twitter import Twitter
+import config as cfg
+import json
+
+
+def add_in_file(filename, text):
+    with open(filename, 'w') as file:
+        file.write('{}'.format(text))
+
+
+def read_from_file(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 
 def main():
-    pass
+    t = Twitter(
+        cfg.twitter.get('consumer_key'), cfg.twitter.get('consumer_secret'),
+        cfg.twitter.get('access_token'),
+        cfg.twitter.get('access_token_secret')
+    )
+
+    accounts = read_from_file('accounts.json')
+    datas = {}
+    for id, followers in accounts.items():
+        if len(followers) == 0:
+            results = t.search(id)
+            datas[str(id)] = results
+            for account in results:
+                datas[str(account)] = []
+        else:
+            datas[str(id)] = followers
+    else:
+        add_in_file('accounts.json', json.dumps(datas))
 
 
 if __name__ == '__main__':
