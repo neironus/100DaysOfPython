5f2d0d5ac6a750cb48295f569589473442b07954
Unknown <JTfbVbcm1K@pas.im>
2018-05-14 08:54:51 +0200

    Python jumpstart 01

---
+++ TalkPython/python-jumpstart/01/program.py
@@ -0,0 +1,10 @@
+def main():
+    print('-----------------------------')
+    print('         HELLO APP')
+    print('-----------------------------')
+
+    name = input('What is your name? ')
+    print('Hello {}'.format(name))
+
+if __name__ == '__main__':
+    main()
