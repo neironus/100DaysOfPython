436e5fa20b5d8e5d450f418485ebfb83cacdf519
Unknown <JTfbVbcm1K@pas.im>
2018-06-16 18:59:45 +0200

    display error if get one

--- TalkPython/97/client/app.py
+++ TalkPython/97/client/app.py
@@ -10,30 +10,36 @@ def main():
 
     gs = GameService()
 
-    id_game = gs.create_game().get('id')
+    try:
+        id_game = gs.create_game().get('id')
+        id_game = 'b28ee491-9cbe-4738-9e08-93ca9885dd7c'
 
-    name = input('Name of player: ')
-    user = gs.find_user(name=name)
+        name = input('Name of player: ')
+        user = gs.find_user(name=name)
 
-    done = False
+        done = False
 
-    while not done:
-        guess = input('Your guess: ')
+        while not done:
+            guess = input('Your guess: ')
 
-        guess_result = gs.make_a_guess(id_player=user.get('id'),
-                                       id_game=id_game, guess=guess)
+            guess_result = gs.make_a_guess(id_player=user.get('id'),
+                                           id_game=id_game, guess=guess)
 
-        status = guess_result.get('status')
+            status = guess_result.get('status')
 
-        if status == 0:
-            print('Your number is to low.')
-        elif status == 2:
-            print('Your number is too high.')
-        else:
-            print('Congratulation your guess the right number.')
-            done = True
+            if status == 0:
+                print('Your number is to low.')
+            elif status == 2:
+                print('Your number is too high.')
+            else:
+                print('Congratulation your guess the right number.')
+                done = True
 
-    print('BYE.')
+        print('BYE.')
+    except Exception as e:
+        print('> Error: {}'.format(
+            e.response.text
+        ))
 
 
 if __name__ == '__main__':
